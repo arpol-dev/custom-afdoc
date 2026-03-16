9401b3d108885335a9f858e6718f4e046bc937e0
default <default@Legion5-15ACH6H-82JU>
2026-03-16 21:12:52 +0100

    [16.0][FIX] correction de tout

--- multi-company-config/models/mailing.py
+++ multi-company-config/models/mailing.py
@@ -3,12 +3,12 @@ from odoo import models, fields, api
 class MassMailing(models.Model):
     _inherit = 'mailing.mailing'
 
-    company_id = fields.Many2one(default=lambda self: self.env.user.company_id if self.env.user.company_id else None)
+    company_id = fields.Many2one(default=lambda self: self.env.company.ids[0] if self.env.company else None)
 
     @api.depends('reply_to_mode')
     def _compute_reply_to(self):
         for mailing in self:
             if mailing.reply_to_mode == 'new' and not mailing.reply_to:
-                mailing.reply_to = self.env.user.company_id.email_formatted
+                mailing.reply_to = self.env.company.email_formatted
             elif mailing.reply_to_mode == 'update':
                 mailing.reply_to = False
--- multi-company-config/models/res_partner.py
+++ multi-company-config/models/res_partner.py
@@ -3,9 +3,9 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    company_id = fields.Many2one(default=lambda self: self.env.user.company_id if self.env.user.company_id else None)
+    company_id = fields.Many2one(default=lambda self: self.env.company.ids[0] if self.env.company else None)
 
 class ResPartnerCategory(models.Model):
     _inherit = 'res.partner.category'
 
-    company_id = fields.Many2one('res.company', 'Company', index=True, default=lambda self: self.env.user.company_id if self.env.user.company_id else None)
+    company_id = fields.Many2one('res.company', 'Company', index=True, default=lambda self: self.env.company.ids[0] if self.env.company else None)
