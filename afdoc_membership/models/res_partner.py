456ddc392c4de1154f9445334dcac3057e07d1b1
ArPol <armand@arpol.fr>
2026-05-30 10:07:03 +0200

    [16.0][UPG] afdoc_membership : improve membership_state computation

--- afdoc_membership/models/res_partner.py
+++ afdoc_membership/models/res_partner.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import date
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -28,3 +29,59 @@ class ResPartner(models.Model):
                 partner.member_products = partner.member_lines.filtered(lambda line: line.state in ['free', 'paid']).mapped('membership_id')
             else:
                 partner.member_products = [(5, 0, 0)]
+
+    # merberstate compute method has flaws in the standard membership module, we redefine it here.
+    @api.depends('member_lines.account_invoice_line',
+                 'member_lines.account_invoice_line.move_id.state',
+                 'member_lines.account_invoice_line.move_id.payment_state',
+                 'member_lines.account_invoice_line.move_id.partner_id',
+                 'free_member',
+                 'member_lines.date_to', 'member_lines.date_from',
+                 'associate_member', 'associate_member.membership_state')
+    def _compute_membership_state(self):
+        today = fields.Date.today()
+        for partner in self:
+            partner.membership_start = self.env['membership.membership_line'].search([
+                ('partner', '=', partner.associate_member.id or partner.id), ('date_cancel', '=', False)
+            ], limit=1, order='date_from').date_from
+            partner.membership_stop = self.env['membership.membership_line'].search([
+                ('partner', '=', partner.associate_member.id or partner.id), ('date_cancel', '=', False)
+            ], limit=1, order='date_to desc').date_to
+            partner.membership_cancel = self.env['membership.membership_line'].search([
+                ('partner', '=', partner.id)
+            ], limit=1, order='date_cancel').date_cancel
+
+            if partner.associate_member:
+                partner.membership_state = partner.associate_member.membership_state
+                continue
+
+            if partner.free_member and partner.membership_state != 'paid':
+                partner.membership_state = 'free'
+                continue
+
+            current_mlines = partner.member_lines.filtered(lambda line: (line.date_to or date.min) >= today and (line.date_from or date.min) <= today)
+            others_mlines = partner.member_lines - current_mlines
+            STATE_ORDER = {
+                'none': 0,
+                'waiting': 1,
+                'invoiced': 2,
+                'canceled': 3,
+                'paid': 4
+            }
+
+            if current_mlines:
+                cur_state = max(
+                    current_mlines.mapped('state'),
+                    key=lambda s: STATE_ORDER.get(s, -1),
+                ) 
+                partner.membership_state = cur_state
+            else:
+                partner.membership_state = 'none'
+            
+            if (not current_mlines or cur_state not in ['free', 'paid']) and others_mlines:
+                old_state = max(
+                    others_mlines.mapped('state'),
+                    key=lambda s: STATE_ORDER.get(s, -1),
+                )
+                if old_state == 'paid':
+                    partner.membership_state = 'old'
